0b6c6f13fc346c7c52d0fd98928bc3e904c3c3ca
tejesh95 <tejeshpapineni95@gmail.com>
2017-05-31 17:20:45 +0000

    Done

---
+++ even_numbers.py
@@ -0,0 +1,8 @@
+def get_even_numbers(input_num):
+    if input_num <= 0:
+        raise ValueError("number must be greater than 0")
+    result = []
+    for i in range(1, input_num + 1):
+        if i%2 == 0:
+            result.append(i)
+    return result

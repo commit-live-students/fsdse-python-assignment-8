4056e58416c23e5355a200d8463cb05c1c13bd3a
PankajMehar <pankaj02mehar@gmail.com>
2017-07-10 05:18:51 +0000

    Done

--- even_numbers.py
+++ even_numbers.py
@@ -1,12 +1,3 @@
-def get_even_numbers(last_Digit):
-
-    Even_Number = []
-    if last_Digit > 0 :
-        for i in range(1,last_Digit) :
-            if i%2==0 :
-                Even_Number.append(i)
-    else :
-        print "Please enter any value above zero!"
-    return Even_Number
-
+def get_even_numbers(m):
+    return [i for i in range(1,m+1) if i%2 == 0]
 get_even_numbers(12)
